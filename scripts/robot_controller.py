74d621906da131f6a245bb33828ea55049164688
Andy Ponce <andy.ponce@outlook.com>
2022-10-27 20:32:14 -0400

    robot now capable of navigating out of canyon

--- scripts/robot_controller.py
+++ scripts/robot_controller.py
@@ -12,6 +12,9 @@ class RobotController:
         self.left_sub = rospy.Subscriber("/ponce/left", Int32, self.left_callback)
         self.right_sub = rospy.Subscriber("/ponce/right", Int32, self.right_callback)
         self.twistCmd = Twist()
+        self.left_right_counter = 0
+        self.left_counter=0
+        self.right_counter=0
         self.left_feeler = 0
         self.right_feeler = 0
         self.rate = rospy.Rate(20)
@@ -25,17 +28,31 @@ class RobotController:
         self.right_feeler = msg.data
     def currentBehavior(self):
         if self.right_feeler == 1 and self.left_feeler == 0:
-            self.twistCmd.angular.z = -0.5
-            self.twistCmd.linear.x = -1.0
+            self.right_counter+=1
+            self.twistCmd.angular.z = -1.1
+            self.twistCmd.linear.x = 0.5
             self.publishTwistCmd()
             #print("right_feeler = 1")
         elif self.left_feeler == 1 and self.right_feeler == 0:
-            self.twistCmd.angular.z = 0.5
-            self.twistCmd.linear.x = -1.0
+            self.left_counter+=1
+            self.twistCmd.angular.z = 1.1
+            self.twistCmd.linear.x = 0.5
             self.publishTwistCmd()
             #print("left_feeler = 1")
         elif self.left_feeler == 1 and self.right_feeler == 1:
-            self.twistCmd.angular.z = 0.0
+            self.left_right_counter+=1
+            if self.left_right_counter >= 3:
+                if self.right_counter > self.left_counter:
+                    self.twistCmd.angular.z = 2.2
+                elif self.left_counter > self.right_counter:
+                    self.twistCmd.angular.z = -2.2
+                else:
+                    self.twistCmd.angular.z = 2.2
+                rospy.Rate(1).sleep
+                self.left_right_counter=0
+                self.left_counter=0
+                self.right_counter=0
+            #self.twistCmd.angular.z = 0.0
             self.twistCmd.linear.x = -1.0
             self.publishTwistCmd()
             #print("left_feeler = 1")
